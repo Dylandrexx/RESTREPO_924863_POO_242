c38151164d1db9e32947f311251540dda295c584
Dylan Restrepo <dylan.restrepo-m@uniminuto.edu.co>
2024-09-21 01:16:42 +0000

    primera practica del lab 7

---
+++ semana7/lab7.py
@@ -0,0 +1,29 @@
+# student = {
+#     "name": "James",
+#     "age": 33
+# }
+# print(student["name"])
+
+
+
+def class_person(name, lastname, id, age, role):
+    return{
+        "name": name,
+        "lasname": lasname,
+        "id": id,
+        "age": age,
+        "role" role
+    }
+def show_objet(objet_):
+    print(show_objet)
+
+def add_person():
+    name= input("Por favor digite el nombre:\n")
+    lasname= input("Por favor digite el apellido:\n")
+    id= int(input("Por favor digite el id:\n"))
+    age= int(input("Por favor digite la edad:\n"))
+    role= int(input("Por favor digite su rol:\n"))
+
+#Agregar personas a una lista.
+#Usar un bucle para agregar usuarios hasta que el usuario decida no agregar mas
+#imprimir el contenido del listado
